b53e3c4cecb4496b7e1de1ea0323c1b8391c627d
Fabiano Silva <80015492+fabiano00760@users.noreply.github.com>
2022-03-09 02:15:40 -0300

    Add files via upload

---
+++ web_html_ccs/javascript.js
@@ -0,0 +1,12 @@
+const a = 7;
+ if (a === 10) {
+   console.log("Valor máximo");
+ } else if (a > 10 ) {
+   console.log("Valor excedido");
+ } else if (a >= 5) {
+   console.log("Valor dentro da média");
+ } else if (a < 5) {
+   console.log("Valor fora da média");
+ } else {
+   console.log("Valor invalido");
+ }
